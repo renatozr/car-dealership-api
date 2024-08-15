a127089d914433c65314e7576385a406b026a837
Renato Zanella Redoglia <renatozr07@gmail.com>
2024-08-15 01:59:40 +0000

    add server doc

---
+++ src/doc/specs.ts
@@ -0,0 +1,22 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+
+const options: swaggerJsDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Car Dealership API',
+      version: '1.0.0',
+      description: 'A REST API that handles vehicle data from a car dealership',
+    },
+    servers: [
+      {
+        url: process.env.SERVER_URL || 'http://localhost:3001',
+      },
+    ],
+  },
+  apis: ['./src/routes/*'],
+};
+
+const specs = swaggerJsDoc(options);
+
+export default specs;
